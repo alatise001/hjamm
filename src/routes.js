79e167a8af4e19a4eca4aaa7802062761095a015
alatise001 <76849022+alatise001@users.noreply.github.com>
2026-07-20 02:32:55 +0100

    social media fix

--- src/routes.js
+++ src/routes.js
@@ -15,4 +15,10 @@ export const routes = {
     livePrayer: "/live-prayer",
 
     aboutUs: "/about-us",
+
+    facebook: "https://www.facebook.com/share/g/1H6ipownfg/?mibextid=wwXIfr",
+    tiktok: "https://www.tiktok.com/@heartsofjesusandm4?_r=1&_t=ZP-974q2DRPple",
+    youtube: "https://youtube.com/@heartsofjesusandmaryminist4802?si=rUhuncpn1iI_Nwde",
+    instagram: "https://www.instagram.com/hjamm_1"
+
 }
